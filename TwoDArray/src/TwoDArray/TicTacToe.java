42a2125523fd39b5d9baead19a3d23bdb0c21a78
sharook-khan-pathan <sharookkhanpathan626@gmail.com>
2025-07-03 12:45:22 +0530

    Updated TicTacToe

--- TwoDArray/src/TwoDArray/TicTacToe.java
+++ TwoDArray/src/TwoDArray/TicTacToe.java
@@ -4,13 +4,7 @@ public class TicTacToe {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		char[][] table = new char[3][3];
-		Scanner sc = new Scanner(System.in);
-		for(int i = 0; i < 3; i++) {
-			for(int j = 0; j < 3; j++) {
-				table[i][j] = sc.next().charAt(0);
-			}
-		}
+		
 	}
 
 }
