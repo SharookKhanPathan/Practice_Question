e63d77bb8e69c629975aa69fe1598f1463e69483
Sharook khan Pathan <124382175+SharookKhanPathan@users.noreply.github.com>
2025-07-09 22:04:22 +0530

    Add files via upload

---
+++ TwoDArray/src/TwoDArray/PaymentProccessingSystem.java
@@ -0,0 +1,35 @@
+package TwoDArray;
+class Payment{
+	void processpayment(){
+		System.out.println("Payment process...");
+	}
+}
+class UPIPayment extends Payment{
+	void processpayment(){
+		System.out.println("UPI Payment Successfull");
+	}
+}
+class CardPayment extends Payment{
+	void processpayment(){
+		System.out.println("Card Payment Successfull");
+	}
+}
+class WalletPayment extends Payment{
+	void processpayment(){
+		System.out.println("Wallet Payment Successfull");
+	}
+}
+public class PaymentProccessingSystem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Payment p;
+		p = new UPIPayment();
+		p.processpayment();
+		p = new CardPayment();
+		p.processpayment();
+		p = new WalletPayment();
+		p.processpayment();
+	}
+
+}
