773ea40bbb6df5cbb0cf3966e23465d46fc01e8b
Sharook khan Pathan <124382175+SharookKhanPathan@users.noreply.github.com>
2025-07-12 22:42:33 +0530

    Add files via upload

---
+++ TwoDArray/src/TwoDArray/DivBy5n11.java
@@ -0,0 +1,14 @@
+4.Write a program to check whether a number is divisible by 5 and 11 or not?
+
+import java.util.*;
+class DivBy5n11{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int num=sc.nextInt();
+		if(num%5==0 && num%11==0)
+			System.out.println("Divisible");
+		else
+			System.out.println("Not Divisible");
+	}
+}
